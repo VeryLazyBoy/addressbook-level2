c9e756d38cfe9fb0f730e1480f5707e377930a22
VeryLazyBoy <frank16874@gmail.com>
2017-02-08 13:23:22 +0800

    Apply lSP

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+/**
+ * A interface for Email, Name, Phone and Address to print strings representing their objects
+ */
+public interface Printable {
+    
+    public String getPrintableString();
+}
--- src/seedu/addressbook/data/person/ReadOnlyPerson.java
+++ src/seedu/addressbook/data/person/ReadOnlyPerson.java
@@ -38,28 +38,23 @@ public interface ReadOnlyPerson {
      */
     default String getAsTextShowAll() {
         final StringBuilder builder = new StringBuilder();
-        final String detailIsPrivate = "(private) ";
-        builder.append(getName())
-                .append(" Phone: ");
-        if (getPhone().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getPhone())
-                .append(" Email: ");
-        if (getEmail().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getEmail())
-                .append(" Address: ");
-        if (getAddress().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getAddress())
-                .append(" Tags: ");
+        builder.append(getPrintableString(getName(), getPhone(), getEmail(), getAddress()));
+        builder.append(" Tags: ");
         for (Tag tag : getTags()) {
             builder.append(tag);
         }
         return builder.toString();
+    }
+    
+    /**
+     * Returns a concatenated version of the printable strings of each object.
+     */
+    default String getPrintableString(Printable...printables) {
+        String result = "";
+        for (Printable printable : printables) {
+            result += printable.getPrintableString();
+        }
+        return result;
     }
 
     /**
